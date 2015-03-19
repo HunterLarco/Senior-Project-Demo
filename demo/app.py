3191324df1a44336a89b30e973c5c64b6739fea6
Hunter John Larco <hjlarco@gmail.com>
2015-03-19 17:39:20 -0400

    Three JS Test

--- demo/app.py
+++ demo/app.py
@@ -6,10 +6,18 @@ import os
 class MainHandler(webapp2.RequestHandler):
   def get(self):
     template_values = {}
-    path = os.path.join(os.path.dirname(__file__), 'mobile.html')
+    path = os.path.join(os.path.dirname(__file__), '3dtest.html')
     self.response.out.write(template.render(path, template_values))
 
 
+class MobileHandler(webapp2.RequestHandler):
+  def get(self):
+    template_values = {}
+    path = os.path.join(os.path.dirname(__file__), 'mobile.html')
+    self.response.out.write(template.render(path, template_values))
+  
+
 app = webapp2.WSGIApplication([
+                ('/mobile/?', MobileHandler),
                 ('/.*', MainHandler)
               ], debug=True)
---
+++ demo/resources/scripts/Neuron.js
@@ -0,0 +1,22 @@
+(function(){
+  
+  function Neuron(){
+    var self = this;
+    var undefined;
+    
+    var weights = [];
+    
+    self.train = Train;
+    
+    function Train(){
+      
+    }
+    
+    (function Constructor(){
+      
+    }).apply(this, arguments);
+  }
+  
+  window.Neuron = Neuron;
+  
+})();
