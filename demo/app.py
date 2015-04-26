8b0a3bc990391d4b7efcf945be2bf321b71ae0e0
Hunter John Larco <hjlarco@gmail.com>
2015-04-26 00:21:46 -0400

    Presentation First Thoughts

--- demo/app.py
+++ demo/app.py
@@ -6,10 +6,18 @@ import os
 class MainHandler(webapp2.RequestHandler):
   def get(self):
     template_values = {}
-    path = os.path.join(os.path.dirname(__file__), 'main.html')
+    path = os.path.join(os.path.dirname(__file__), 'controller.html')
+    self.response.out.write(template.render(path, template_values))
+
+
+class PresentationHandler(webapp2.RequestHandler):
+  def get(self):
+    template_values = {}
+    path = os.path.join(os.path.dirname(__file__), 'presentation.html')
     self.response.out.write(template.render(path, template_values))
 
 
 app = webapp2.WSGIApplication([
-                ('/.*', MainHandler)
+                ('/presentation/?', PresentationHandler),
+                ('/controller/?', MainHandler)
               ], debug=True)
